d0356e535b82b1785bc317828f82ee8e3815bb8c
Halil K <64667314+Byozi@users.noreply.github.com>
2023-05-19 22:33:58 +0300

    Update admins.py

--- YukkiMusic/utils/decorators/admins.py
+++ YukkiMusic/utils/decorators/admins.py
@@ -27,7 +27,7 @@ def AdminRightsCheck(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    "Bot bakıma alınmış görünüyor. Daha sonra deneyiniz..."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -84,7 +84,7 @@ def AdminActual(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time..."
+                    Bot bakıma alınmış görünüyor. Daha sonra deneyiniz..."
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
@@ -129,7 +129,7 @@ def ActualAdminCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    "Bot is under maintenance. Please wait for some time...",
+                    "Bot bakıma alınmış görünüyor. Daha sonra deneyiniz...",
                     show_alert=True,
                 )
         try:

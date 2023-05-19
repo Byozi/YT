1f8a3c89facb6fc271759015429f75f662af98a0
Halil K <64667314+Byozi@users.noreply.github.com>
2023-05-19 18:50:33 +0300

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -53,23 +53,23 @@ class Userbot(Client):
         )
 
     async def start(self):
-        LOGGER(__name__).info(f"Starting Assistant Clients")
+        LOGGER(__name__).info(f"Asistanlar Göreve Çağırıldı!")
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
-                await self.one.join_chat("TheYukki")
-                await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("")
+                await self.one.join_chat("")
+                await self.one.join_chat("")
             except:
                 pass
             assistants.append(1)
             try:
                 await self.one.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "Asistana ulaşıldı, Görevini Yapmak İçin Hazır!"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    f"Assistan 1 log grubuna erişemedi. Asistanınızı log grubunuza eklediğinizden ve yönetici olarak atadığınızdan emin olun!"
                 )
                 sys.exit()
             get_me = await self.one.get_me()
@@ -88,19 +88,19 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
-                await self.two.join_chat("TheYukki")
-                await self.two.join_chat("YukkiSupport")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(2)
             try:
                 await self.two.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "2 numaralı Asistana ulaşıldı, Görevini Yapmak İçin Hazır!"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    f"Assistan 2 log grubuna erişemedi. Asistanınızı log grubunuza eklediğinizden ve yönetici olarak atadığınızdan emin olun!"
                 )
                 sys.exit()
             get_me = await self.two.get_me()
@@ -119,19 +119,19 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYM")
-                await self.three.join_chat("TheYukki")
-                await self.three.join_chat("YukkiSupport")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(3)
             try:
                 await self.three.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "3 numaralı Asistana ulaşıldı, Görevini Yapmak İçin Hazır!"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    f"Assistan 3 log grubuna erişemedi. Asistanınızı log grubunuza eklediğinizden ve yönetici olarak atadığınızdan emin olun!"
                 )
                 sys.exit()
             get_me = await self.three.get_me()
@@ -150,19 +150,19 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYM")
-                await self.four.join_chat("TheYukki")
-                await self.four.join_chat("YukkiSupport")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(4)
             try:
                 await self.four.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "4 numaralı Asistana ulaşıldı, Görevini Yapmak İçin Hazır!"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    f"Assistan 4  log grubuna erişemedi. Asistanınızı log grubunuza eklediğinizden ve yönetici olarak atadığınızdan emin olun! "
                 )
                 sys.exit()
             get_me = await self.four.get_me()
@@ -181,19 +181,19 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYM")
-                await self.five.join_chat("TheYukki")
-                await self.five.join_chat("YukkiSupport")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
+                await self.two.join_chat("")
             except:
                 pass
             assistants.append(5)
             try:
                 await self.five.send_message(
-                    config.LOG_GROUP_ID, "Assistant Started"
+                    config.LOG_GROUP_ID, "5 numaralı Asistana ulaşıldı, Görevini Yapmak İçin Hazır!"
                 )
             except:
                 LOGGER(__name__).error(
-                    f"Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
+                    f"Assistan 5 log grubuna erişemedi. Asistanınızı log grubunuza eklediğinizden ve yönetici olarak atadığınızdan emin olun!"
                 )
                 sys.exit()
             get_me = await self.five.get_me()
